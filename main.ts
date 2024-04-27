a00a83db4d71c123db75b04eb2f1e68a4e9847c5
hj022975 <67865647+hj022975@users.noreply.github.com>
2024-04-27 14:29:56 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,6 @@ namespace screenMagic {
     //% block="plot at $index"
     //% index.min=0 index.max=24
     export function plotAt(index: number): void {
-        index |= 0
         const y = Math.floor(index / 5)
         const x = Math.floor(index % 5)
         led.plot(x, y)
