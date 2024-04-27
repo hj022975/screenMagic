bc3377d4235719e1556bc0e16790b1ab01326632
hj022975 <67865647+hj022975@users.noreply.github.com>
2024-04-27 13:52:01 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic {
+    /**
+     * Address LEDs linearly row first
+     */
+    //% blockId=screenmagicplotat
+    //% block="plot at $index"
+    //% index.min=0 index.max=24
+    export function plotAt(index: number): void {
+        index |= 0
+        const y = Math.floor(index / 5)
+        const x = Math.floor(index % 5)
+        led.plot(x, y)
+    }
+}
